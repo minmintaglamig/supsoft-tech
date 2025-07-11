f50c051ece701ca43342d5ad85863ba19062a405
CurlyAsh <azhesmangalino@gmail.com>
2025-07-11 14:46:19 +0800

    sa wakas tapos na
    tapos na

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '4a7ec8672dc440327559d48e89afce26983d290e',
+        'reference' => '93f3fc46127288260902615869fe068167f23cc1',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -442,7 +442,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '4a7ec8672dc440327559d48e89afce26983d290e',
+            'reference' => '93f3fc46127288260902615869fe068167f23cc1',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
